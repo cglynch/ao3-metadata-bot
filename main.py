f5a56c5ceee2063bc0f730c7ab3ec08289a41437
clynch <13966523+cglynch@users.noreply.github.com>
2021-01-23 15:50:13 +0000

    added Procfile

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+# test_bot.py
+import os
+import utils
 from ao3 import AO3
-api = AO3()
-test_work = api.work(id = "28885413")
-print(test_work.fandoms)
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    url_section = "https://archiveofourown.org/works/"
+    if url_section in message.content:
+        id = utils.work_id_from_url(message.content)
+        api = AO3()
+        work = api.work(id=id)
+        output_list = [work.fandoms[0], work.warnings[0], work.rating[0]]
+        response = " | ".join(output_list)
+        await message.channel.send(response)
+
+client.run(TOKEN)
